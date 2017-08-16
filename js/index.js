b3f656eefbad4b87b4f5180c0a41e72e9be9532f
503945930 <503945930@qq.com>
2017-08-16 14:41:36 +0800

    add moment.js

--- js/index.js
+++ js/index.js
@@ -6,19 +6,6 @@
  }, 1000)
 
  function getNowFormatDate () {
-   var date = new Date()
-   var seperator1 = '-'
-   var seperator2 = ':'
-   var month = date.getMonth() + 1
-   var strDate = date.getDate()
-   if (month >= 1 && month <= 9) {
-     month = '0' + month
-   }
-   if (strDate >= 0 && strDate <= 9) {
-     strDate = '0' + strDate
-   }
-   var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
-            ' ' + date.getHours() + seperator2 + date.getMinutes() +
-            seperator2 + date.getSeconds()
-   return currentdate
+   console.log('new Date()', new Date())
+   return window.moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
  }
